a9424897abf6510a069898bc40772bc4600d2cc7
Melis Kırık <152122959+meliskirik@users.noreply.github.com>
2024-05-18 20:19:25 +0300

    My first codes, FindMinimum and HelloWorld

---
+++ lab1/FindMinimum.java
@@ -0,0 +1,14 @@
+
+public class FindMinimum {
+
+	public static void main(String[] args) {
+		int number1 = 10;
+		int number2 = 15;
+		
+		int the_minimum = number1 < number2 ? number1 : number2;
+		System.out.println("the min is " + the_minimum);
+		}
+
+	}
+
+
