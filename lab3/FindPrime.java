19cdd3c314e370df73988662e9b3a3f3aff6e860
Melis Kırık <meliskirik33@gmail.com>
2024-05-20 21:32:21 +0300

    FindPrime and GuessNumber with Java

---
+++ lab3/FindPrime.java
@@ -0,0 +1,21 @@
+package lab3;
+
+public class FindPrime {
+    public static void main(String[] args) {
+        int given = 30;
+        for (int number = 2; number < given; number++) {
+            boolean isPrime = true;
+
+            for (int divisor = 2; divisor < number; divisor++) {
+                if (number % divisor == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(number);
+            }
+        }
+
+    }
+}
