c99b043ed56d1d12397fb7338b78421372c6b2c9
Melis Kırık <meliskirik33@gmail.com>
2024-05-23 20:52:31 +0300

    Delete lab6 directory.

--- lab6/PointTest.java
+++
@@ -1,8 +0,0 @@
-
-public class PointTest {
-    public static void main(String[] args) {
-        Point p1 = new Point(3, 4);
-        Point p2 = new Point(3, 4);
-        System.out.println("The distance between these two points is " + p2.distance(p1));
-    }
-}
