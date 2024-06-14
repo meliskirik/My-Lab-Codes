ae5455928676522ee7b69fe9aeeeb39f12c9202d
Melis Kırık <meliskirik33@gmail.com>
2024-06-14 21:24:32 +0300

    Shapes with Java

---
+++ lab11/drawing/version1/shapes/Rectangle.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class Rectangle extends Shape {
+
+    double sideA, sideB;
+
+    public Rectangle (double sideA, double sideB){
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+    @Override
+    public double area() {
+        return this.sideA*this.sideB;
+    }
+}
